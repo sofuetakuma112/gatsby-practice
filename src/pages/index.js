ed4b71cd9b65c861221bf0ab6b7abea47b5d958e
sofuetakuma <kawahagi0620@gmail.com>
2022-02-18 01:19:55 +0900

    画像最適化

--- src/pages/index.js
+++ src/pages/index.js
@@ -167,6 +167,13 @@ const Home = ({ data }) => {
             </li>
           </ul>
         </div>
+        <div className="back">
+          <Img
+            fluid={data.pattern.childImageSharp.fluid}
+            alt=""
+            style={{ height: "100%" }}
+          />
+        </div>
       </footer>
     </div>
   )
@@ -207,6 +214,13 @@ export const query = graphql`
     berry: file(relativePath: { eq: "berry.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    pattern: file(relativePath: { eq: "pattern.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1920, quality: 90) {
           ...GatsbyImageSharpFluid_withWebp
         }
       }
